e048ed234fbf2a07b8abffc9d4a1e419a1a014bd
MajaKomic <komic.maja@gmail.com>
2023-04-21 21:02:52 +0200

    Premaknila podatke tabel vsako v svojo datoteko in v mapo podatki.

---
+++ podatki/storitev.sql
@@ -0,0 +1,20 @@
+-- Active: 1682070737195@@baza.fmf.uni-lj.si@5432@sem2023_klarat
+insert into Storitev(ime_storitve, trajanje, cena, stroski) values('žensko striženje', 30,35,10 );
+insert into Storitev(ime_storitve, trajanje, cena, stroski) values('moško striženje', 20,25,5 );
+insert into Storitev(ime_storitve, trajanje, cena, stroski) values('pedikura', 30,35,5 );
+insert into Storitev(ime_storitve, trajanje, cena, stroski) values('manikura', 45,40,20 );
+insert into Storitev(ime_storitve, trajanje, cena, stroski) values('barvanje', 60,70,30 );
+insert into Storitev(ime_storitve, trajanje, cena, stroski) values('depilacija nog', 15,40,10 );
+insert into Storitev(ime_storitve, trajanje, cena, stroski) values('depilacija bikini predela', 10,20,5 );
+insert into Storitev(ime_storitve, trajanje, cena, stroski) values('depilacija pazduh', 5,15,5 );
+insert into Storitev(ime_storitve, trajanje, cena, stroski) values('depilacija celega telesa', 45,50,10 );
+insert into Storitev(ime_storitve, trajanje, cena, stroski) values('ličenje', 45,50,20 );
+insert into Storitev(ime_storitve, trajanje, cena, stroski) values('fen frizura', 30,35,10 );
+insert into Storitev(ime_storitve, trajanje, cena, stroski) values('masaža', 50,35,5 );
+insert into Storitev(ime_storitve, trajanje, cena, stroski) values('laminacija obrvi', 30,35,5 );
+insert into Storitev(ime_storitve, trajanje, cena, stroski) values('podaljševanje trepalnic', 45,50,20 );
+insert into Storitev(ime_storitve, trajanje, cena, stroski) values('medicinska pedikura', 20,40,10 );
+insert into Storitev(ime_storitve, trajanje, cena, stroski) values('lasersko odstranjevanje dlak', 60,80,20 );
+insert into Storitev(ime_storitve, trajanje, cena, stroski) values('urejanje brade', 20,30,10 );
+insert into Storitev(ime_storitve, trajanje, cena, stroski) values('nega las in lasišča', 25,20,5 );
+
